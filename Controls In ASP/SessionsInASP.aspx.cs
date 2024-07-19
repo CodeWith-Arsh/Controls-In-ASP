d4850b57f7839d1625691b292da422240d1f94f5
arshid <arshid.ahmad1@gmail.com>
2024-07-19 16:23:11 +0530

    Sessions in PHP

---
+++ Controls In ASP/SessionsInASP.aspx.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace Controls_In_ASP
+{
+    public partial class SessionsInASP : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void login_Click(object sender, EventArgs e)
+        {
+            if (password.Text == "qwe123")
+            {
+                Session["email"] = email.Text;
+
+            }
+            if (Session["email"] != null)
+            {
+                Label3.Text = "This email is stored to the session";
+                Label4.Text = Session["email"].ToString();
+            }
+        }
+    }
+}
